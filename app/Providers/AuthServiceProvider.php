df6a50d6efdcaa0877f0ca606c1bf4a84f340507
riskadwinuraini <riskadwi583@gmail.com>
2022-04-26 12:33:29 +0700

    add role in table user & register gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('isAdmin', function ($user)
+        {
+           return $user->role = 'admin'; 
+        });
+
+        Gate::define('isManager', function ($user)
+        {
+           return $user->role = 'manager'; 
+        });
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,18 +15,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
         $users = [
             [
                 'name' =>'Nelly Maulia',
                 'email' =>'nelly123@gmail.com',
                 'password' => bcrypt('nelly123'),
                 'sim' => 'sim A',
+                'role' => 'admin'
             ],
             [
                 'name' =>'Eggi Permana',
                 'email' =>'eggi_p098@gmail.com',
                 'password' => bcrypt('eggi098'),
                 'sim' => 'sim A',
+                'role' => 'manager'
             ]
         ];
 
